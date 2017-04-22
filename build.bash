d5c3fcee066605ad4693697ff447e9449a678177
Adam Cavendish <GetbetterABC@yeah.net>
2017-04-22 15:15:10 +0800

    Add glfw example

--- build.bash
+++ build.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for project in project*; do
+CI_PROJECTS=(
+  project1-integrate_reason_with_old_project
+  project2-depend_on_other_reason_project
+  project3-rewrite_project1_with_reason
+  project4-header_only_library
+  project5-multi_exe_dep_on_common_lib
+  # project6-cuda_example                    # Travis: no CUDA support
+  project7-glfw_example
+)
+
+for project in ${CI_PROJECTS[*]}; do
   echo "Building Project: $project"
   cd $project/
   rm -rf build/
